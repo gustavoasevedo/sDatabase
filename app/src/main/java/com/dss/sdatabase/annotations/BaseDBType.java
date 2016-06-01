135b4fe62227b15b3761cd4db38b119a3b3f252f
Gustavo Asevedo Vieira <gustavo.asevedo@gmail.com>
2016-06-01 11:56:32 -0300

    First Commit

---
+++ app/src/main/java/com/dss/sdatabase/annotations/BaseDBType.java
@@ -0,0 +1,15 @@
+package com.dss.sdatabase.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Created by digipronto on 29/02/16.
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface BaseDBType {
+
+    String value();
+
+}
---
+++ app/src/main/java/com/dss/sdatabase/exceptions/InvalidTypeException.java
@@ -0,0 +1,11 @@
+package com.dss.sdatabase.exceptions;
+
+/**
+ * Created by digipronto on 01/03/16.
+ */
+public class InvalidTypeException extends Exception {
+
+    public InvalidTypeException(String message) {
+        super(message);
+    }
+}
